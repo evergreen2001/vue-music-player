751171f4cd869feafb731f46239aa9ba1f3bc85e
unknown <dicksonevergreenido@gmail.com>
2020-08-09 15:11:01 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <h1>MUSIC APP</h1>
+
+    <h2>
+      {{current.title}} -
+      <span>{{current.artist}}</span>
+    </h2>
+
+
+    <button>Prev</button>
+    <button>Pause</button>
+    <button>play</button>
+
+    <button>Next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      index: 0,
+      current: {},
+
+      songs: [
+        {
+          title: "billonaire",
+          artist: "Teni",
+          src: require("./assets/Teni-Billionaire.mp3")
+        },
+
+        {
+          title: "ojoro",
+          artist: "Terri",
+          src:require("./assets/Terri-Ojoro.mp3") 
+        }
+      ],
+      player: new Audio()
+    };
+  },
+
+  created() {
+    this.current = this.songs[0];
+    this.player.src = this.current.src;
+    this.player.play();
+  },
+  components: {}
+};
+</script>
+
+<style>
+
+*{
+  margin:0;
+  padding:0
+}
+body{
+
+margin:auto
+}
+button{
+
+  margin:10px;
+}
+</style>
