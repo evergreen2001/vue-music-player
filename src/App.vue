de420e3b7268db1ba8fa26eaacc7baa3439736f8
unknown <dicksonevergreenido@gmail.com>
2020-08-09 16:46:57 +0100

    functionality ready

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,19 @@
       <span>{{current.artist}}</span>
     </h2>
 
-    <button>Prev</button>
-    <button v-if="!isNowPlaying" @click="pause()">Pause</button>
-    <button v-else @click="play()">play</button>
+    <button @click="prev">Prev</button>
+    <button v-if="!isNowPlaying" @click="pause">Pause</button>
+    <button v-else @click="play">play</button>
 
-    <button>Next</button>
+    <button @click="next">Next</button>
+
+    <section>
+      <h3>{{username}} PLaylist</h3>
+
+
+
+      <button v-for="song in songs" :key="song.src" @click="play(song)"> {{song.artist}}  || {{song.title}}</button>
+    </section>
   </div>
 </template>
 
@@ -41,30 +49,61 @@ export default {
       ],
       player: new Audio(),
       isNowPlaying: true,
-      song: null
+      username: ""
     };
   },
 
   created() {
+    // this.username = prompt("enter your name ");
+
+    // console.log(this.username);
     this.current = this.songs[this.index];
     this.player.src = this.current.src;
   },
   components: {},
   methods: {
     play(song) {
-      if ( typeof song.src != "undefined") {
+      if (typeof song.src != "undefined") {
         this.current = song;
         this.player.src = this.current.src;
+
+     
       }
-      this.player.play();
+         this.player.play();
       this.isNowPlaying = false;
     },
 
     pause() {
       this.player.pause();
       this.isNowPlaying = true;
-    }
+    } , 
+
+    next(){
+
+this.index++
+      if(this.index > this.songs.length - 1){
+        this.index = 0
+
+      }
+
+      this.current  = this.songs[this.index] ; 
+      this.play(this.current)
+    } , 
+
+     prev(){
+
+    this.index --;
+      if(this.index < 0){
+        this.index = this.songs.length -1
+
+      }
+
+      this.current  = this.songs[this.index] ; 
+      this.play(this.current)
+    
   }
+  } , 
+ 
 };
 </script>
 
